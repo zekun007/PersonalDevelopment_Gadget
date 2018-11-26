40993d68da756bad84f3b4428fce7e439ddd4932
zekun007 <1522278241@qq.com>
2018-11-27 00:17:18 +0800

    ssw

--- exceltools/src/main/java/com/zekun/dev/exceltools/ExceltoolsApplication.java
+++ exceltools/src/main/java/com/zekun/dev/exceltools/ExceltoolsApplication.java
@@ -2,11 +2,27 @@ package com.zekun.dev.exceltools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
 @SpringBootApplication
 public class ExceltoolsApplication {
 //	private Logger logger = LoggerFactory.getLogger(getClass());
 	public static void main(String[] args) {
 		SpringApplication.run(ExceltoolsApplication.class, args);
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//文件最大
+		factory.setMaxFileSize("1024MB"); //KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("1024MB");
+		return factory.createMultipartConfig();
 	}
 }
